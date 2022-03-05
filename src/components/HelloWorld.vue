f24f6e04ff98cdb85de5810f2098540eb605139d
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2022-03-04 19:52:30 -0600

    diseño al boton

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,19 @@
 <template>
     <div class="estadistica">      
 
+            <div class="botones">
+              <p> Agregar </p>
+              <button class="btn btn-success" @click="agregar"> Agregar </button>
+              <p> Eliminar </p>
+              <button class="btn btn-danger" @click="eliminar"> Eliminar </button>
+            </div>
+
             <div v-for="(suc) in sucursales" :key="suc">          
                
               <div class="border border-4">
                 {{suc.name}}
                 <p>
-                  <button :class="{conectado: suc.stateconection == true, desconectado: suc.stateconection == false}" disabled></button>                    
+                  <button :class="{conectado: suc.stateconection == true, desconectado: suc.stateconection == false}" disabled>Status</button>                    
                 </p>
                 
               </div>
@@ -50,10 +57,20 @@
       width: 100%;
       height: 100%;
       background-color: #f2f2f2;
-
-
-
     }
+
+    .botones{
+      display: flex;
+      justify-content: space-around;
+      text-align: center;
+      flex-wrap: wrap;
+      margin: 20px;     
+      padding: 10px;
+      width: 100%;
+      height: 100%;
+      background-color: #f2f2f2;
+    }
+
 
     .border{
       border: 1px solid #ccc;
@@ -65,22 +82,14 @@
 
     }
 
-    button{
-      background-color: #f8f8f8;
-      border: 1px solid #ccc;
-      border-radius: 4px;
-      width: 80px;
-      height: 30px;
-      margin-top: 10px;
-      margin-left: 10px;
-    }
-
     .desconectado{
-        margin-top: 20px;
+      border-radius: 4px;
+      margin-top: 20px;
         background-color: #ff0000;
     }
 
     .conectado{
+      border-radius: 4px;
       margin-top: 20px;
         background-color: #00ff00;
     }
